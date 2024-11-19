3859412ec86d31fb21d5304d39ed468513347660
jbgaela <jbgaela@gmail.com>
2024-11-20 01:12:30 +0800

    Change back

--- src/features/user/components/TemplatePointers.tsx
+++ src/features/user/components/TemplatePointers.tsx
@@ -2,9 +2,7 @@ const TemplatePointers = () => {
   return (
     <>
       <h1 className="text-2xl mt-8 font-bold">React Web App Demo</h1>
-      <p className="py-2 mt-4">
-        Login Demo React App Hello JEAN BENEDICT GAELA
-      </p>
+      <p className="py-2 mt-4">Login Demo React App</p>
     </>
   );
 };
